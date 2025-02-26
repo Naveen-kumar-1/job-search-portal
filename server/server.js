7f9ff98941e1277e80cb981357035f707d1d162a
Naveen-kumar-1 <codemavericknk@gmail.com>
2025-02-27 02:13:36 +0530

    changes maded

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
   });
 
-app.post('/webkooks',clerkWebhooks)  
+app.post('/webhooks',clerkWebhooks)  
 
 
 
